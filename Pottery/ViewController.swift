926f7633b7f20050517da55120d647fcda10a9c9
bdvzt <budaevazay@mail.ru>
2026-03-11 02:53:52 +0700

    Создала проект

---
+++ Pottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pottery
+//
+//  Created by Zayata Budaeva on 11.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
